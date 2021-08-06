24e239cf3ad8352885e08cb1efcf0d2492dd61ae
luisaLuna <luisacasaslu@gmail.com>
2021-08-06 16:33:01 -0500

    Header component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,11 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Header from './header.js';
 
 function App() {
-  return (
-    <header class="header">
-    <div class="wrapper">
-      <div class="header-grid">
-        <div>
-          <h1>Social Media Dashboard</h1>
-          <p class="header-total">Total Followers: 23, 004</p>
-        </div>
-        <div class="dark-mode">
-          <p class="dark-mode-title">Dark Mode</p>
-          <input type="checkbox" class="checkbox" id="checkbox"/>
-          <label class="switch" for="checkbox"></label>
-        </div>
-      </div>
-    </div>
-  </header>
-  );
+  return <Header/>;
+     
+  
 }
 
 export default App;
